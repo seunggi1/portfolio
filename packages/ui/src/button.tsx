2288224a7c96678bf707e7c956d4ddfec201cbcf
kimsg <ksg9396@gmail.com>
2025-01-21 17:34:44 +0900

    공통 UI - button 추가 및 설정 값 수정

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,23 @@
+import { type HTMLAttributes } from 'react';
+
+export function Button({
+	className,
+	children,
+	onClick,
+	...props
+}: HTMLAttributes<HTMLButtonElement>): JSX.Element {
+	return (
+		<button
+			// TODO: 시멘틱 컬러 시스템 적용
+			className={[
+				'ui-bg-cyan-300 ui-text-white hover:ui-bg-cyan-400',
+				className,
+			].join(' ')}
+			onClick={onClick}
+			type="button"
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
