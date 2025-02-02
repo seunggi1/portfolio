9bd0fbe59dcd603e900f59e89f37a7b437b54201
kimsg <ksg9396@gmail.com>
2025-02-02 22:48:47 +0900

    Config - 테일윈드 확장 컬러 삭제

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -3,16 +3,7 @@ import type { Config } from 'tailwindcss';
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
 	theme: {
-		extend: {
-			colors: {
-				primary: '#2194f3',
-				'primary-light': '#4da9f5',
-				'primary-dark': '#1767aa',
-				secondary: '#ffc928',
-				'secondary-light': '#ffd353',
-				'secondary-dark': '#b28c1c',
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
