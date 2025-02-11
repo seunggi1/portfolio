31428a5e33fb90fdc6781e2112b2a80d440e65ab
kimsg <ksg9396@gmail.com>
2025-02-11 16:22:27 +0900

    Config - daisyui 공통 패키지로 갖도록 이동

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from 'tailwindcss';
+import daisyUI from 'daisyui';
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	daisyui: {
+		themes: ['light', 'dark', 'corporate'],
+	},
+	plugins: [daisyUI],
 };
 export default config;
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,17 +1,10 @@
 import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
-import daisyUI from 'daisyui';
 
-const config: Pick<
-	Config,
-	'prefix' | 'presets' | 'content' | 'plugins' | 'daisyui'
-> = {
+const config: Pick<Config, 'prefix' | 'presets' | 'content' | 'plugins'> = {
 	content: ['./src/**/*.tsx'],
 	presets: [sharedConfig],
-	daisyui: {
-		themes: ['light', 'dark', 'corporate'],
-	},
-	plugins: [daisyUI],
+	plugins: [],
 };
 
 export default config;
