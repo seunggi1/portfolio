cc511cb87e14f983149da3a3f20d4cffb39ea04f
kimsg <ksg9396@gmail.com>
2025-02-14 19:33:50 +0900

    맨몸운동헬퍼 - axios 추가 및 axios를 이용한 httpClient 추가

---
+++ apps/calisthenics-helper/src/api/httpClient.ts
@@ -0,0 +1,72 @@
+import axios, { AxiosInstance, AxiosPromise } from 'axios';
+
+type HttpMethod = 'GET' | 'POST' | 'UPDATE' | 'PATCH' | 'DELETE';
+
+type HttpParams = Record<string, string | number>;
+
+class HttpClient {
+	readonly method: HttpMethod;
+	readonly url: string;
+	baseURL: string;
+	timeout: number;
+	data?: unknown;
+	params?: HttpParams;
+
+	constructor(method: HttpMethod, url: string) {
+		this.method = method;
+		this.url = url;
+		this.baseURL = '/';
+		this.timeout = 5000;
+	}
+
+	call<T>(): AxiosPromise<T> {
+		const http: AxiosInstance = axios.create();
+		return http.request<T>({ ...this });
+	}
+}
+
+export class HttpClientBuilder {
+	private readonly _instance: HttpClient;
+
+	constructor(method: HttpMethod, url: string) {
+		this._instance = new HttpClient(method, url);
+	}
+
+	static get(url: string): HttpClientBuilder {
+		return new HttpClientBuilder('GET', url);
+	}
+	static post(url: string): HttpClientBuilder {
+		return new HttpClientBuilder('POST', url);
+	}
+
+	static update(url: string): HttpClientBuilder {
+		return new HttpClientBuilder('UPDATE', url);
+	}
+
+	static patch(url: string): HttpClientBuilder {
+		return new HttpClientBuilder('PATCH', url);
+	}
+
+	static delete(url: string): HttpClientBuilder {
+		return new HttpClientBuilder('DELETE', url);
+	}
+
+	baseURL(url: string): HttpClientBuilder {
+		this._instance.baseURL = url;
+		return this;
+	}
+
+	params(params: HttpParams): HttpClientBuilder {
+		this._instance.params = params;
+		return this;
+	}
+
+	timeout(timeout: number): HttpClientBuilder {
+		this._instance.timeout = timeout;
+		return this;
+	}
+
+	call<T>(): AxiosPromise<T> {
+		return this._instance.call();
+	}
+}
