53782fb08610367e96057d494501ccb24b674f1f
kimsg <ksg9396@gmail.com>
2025-02-14 19:35:22 +0900

    맨몸운동헬퍼 - 루틴 api 레이어 추가

---
+++ apps/calisthenics-helper/src/api/Routines.ts
@@ -0,0 +1,36 @@
+import type { Routine, RoutineDetail } from '@/types/routine';
+import { HttpClientBuilder } from './httpClient';
+
+function getRoutineURL(id?: string) {
+	const ROUTINE_URL = 'routines/api';
+
+	const urls = [ROUTINE_URL];
+
+	if (id) {
+		urls.push(id);
+	}
+
+	return urls.join('/');
+}
+
+export async function fetchRoutines(): Promise<Routine[]> {
+	const res = await HttpClientBuilder.get(getRoutineURL()).call<Routine[]>();
+
+	return res.data;
+}
+
+export async function fetchRoutineDetailById(
+	id: string
+): Promise<RoutineDetail> {
+	const res = await HttpClientBuilder.get(
+		getRoutineURL(id)
+	).call<RoutineDetail>();
+
+	return res.data;
+}
+
+export async function fetchRoutineById(id: string): Promise<Routine> {
+	const res = await HttpClientBuilder.get(getRoutineURL(id)).call<Routine>();
+
+	return res.data;
+}
