691ac30ef43df4342a691183bf45b5d3b66c0436
kimsg <ksg9396@gmail.com>
2025-02-18 19:20:58 +0900

    맨몸운동헬퍼 - 사용하지않는 mock client 삭제

--- apps/calisthenics-helper/src/services/index.ts
+++ apps/calisthenics-helper/src/services/index.ts
@@ -1,5 +1,4 @@
 import { ServiceClient } from './base/serviceClient';
-import { MockServiceClient } from './mock/mockServiceClient';
 import { SupabaseServiceClient } from './supabase/supabaseServiceClient';
 import { createClient as createSupabaseClient } from './supabase/server';
 
@@ -9,8 +8,6 @@ export async function getServiceClient(): Promise<ServiceClient> {
 	switch (service) {
 		case 'supabase':
 			return new SupabaseServiceClient(await createSupabaseClient());
-		case 'test':
-			return new MockServiceClient();
 		default:
 			throw new Error('Unsupported service');
 	}
