c2f3b84e9642540b0ada57c3e8ebfc9ca4e488cc
kimsg <ksg9396@gmail.com>
2025-02-20 21:58:51 +0900

    맨몸운동헬퍼 - 사용하지않는 인증 코드 삭제

--- apps/calisthenics-helper/src/api/auth/auth.ts
+++ apps/calisthenics-helper/src/api/auth/auth.ts
@@ -1,24 +1,4 @@
 import { getServiceClient } from '@/services';
-import { HttpClientBuilder } from '../httpClient';
-
-enum AuthType {
-	SIGN_UP = 1,
-	SIGN_IN,
-	SIGN_OUT,
-}
-
-function getAuthURL(type: AuthType) {
-	switch (type) {
-		case AuthType.SIGN_UP:
-			return 'api/signup';
-		case AuthType.SIGN_IN:
-			return 'api/signin';
-		case AuthType.SIGN_OUT:
-			return 'api/signout';
-		default:
-			throw new Error('올바르지않은 타입입니다.');
-	}
-}
 
 export async function checkDisplayName(displayName: string): Promise<boolean> {
 	const client = await getServiceClient();
@@ -36,32 +16,18 @@ export async function signUp(
 	displayName: string,
 	email: string
 ): Promise<boolean> {
-	// const result = await HttpClientBuilder.post(getAuthURL(AuthType.SIGN_IN))
-	// 	.data({ email })
-	// 	.call<boolean>();
-
-	// return true;
 	const client = await getServiceClient();
 
 	return client.signUp(email, displayName);
 }
 
 export async function signIn(email: string): Promise<boolean> {
-	// const result = await HttpClientBuilder.post(getAuthURL(AuthType.SIGN_IN))
-	// 	.data({ email })
-	// 	.call<boolean>();
-
-	// return true;
 	const client = await getServiceClient();
 
 	return client.signIn(email);
 }
 
 export async function signOut(): Promise<boolean> {
-	// const result = await HttpClientBuilder.post(
-	// 	getAuthURL(AuthType.SIGN_OUT)
-	// ).call<boolean>();
-
 	const client = await getServiceClient();
 	client.signOut();
 
