8087a49cf7063465ec7e2199792c7a583a242885
kimsg <ksg9396@gmail.com>
2025-03-08 21:58:57 +0900

    맨몸운동헬퍼 - 모달 UI 수정

--- apps/calisthenics-helper/src/components/common/modal/Modal.tsx
+++ apps/calisthenics-helper/src/components/common/modal/Modal.tsx
@@ -1,13 +1,14 @@
-import { PropsWithChildren, RefAttributes, useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import { PropsWithChildren, useEffect } from 'react';
 
 type Props = {
 	title: string;
+	onClose: () => void;
 } & PropsWithChildren;
 
 const MODAL_ID = 'modal-1';
 
-export default function Modal({ title, children }: Props) {
+export default function Modal({ title, children, onClose }: Props) {
 	useEffect(() => {
 		(document.getElementById(MODAL_ID) as HTMLDialogElement).showModal();
 	}, []);
@@ -15,11 +16,14 @@ export default function Modal({ title, children }: Props) {
 	return (
 		<div>
 			{createPortal(
-				<dialog id={MODAL_ID} className="modal" data-keyboard="false">
+				<dialog id={MODAL_ID} className="modal">
 					<div className="modal-box">
 						<h3 className="font-bold text-lg">{title}</h3>
 						<div className="modal-action">{children}</div>
 					</div>
+					<form method="dialog" className="modal-backdrop">
+						<button onClick={onClose}>close</button>
+					</form>
 				</dialog>,
 				document.body
 			)}
