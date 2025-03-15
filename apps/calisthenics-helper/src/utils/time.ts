32c93a97625308a53606d48d27afe817c38bacb5
kimsg <ksg9396@gmail.com>
2025-03-15 23:06:01 +0900

    맨몸운동헬퍼 - 지난시간 구하는 유틸리티 함수 추가

--- apps/calisthenics-helper/src/utils/time.ts
+++ apps/calisthenics-helper/src/utils/time.ts
@@ -21,3 +21,34 @@ function millisecondsToSeconds(milliseconds: number) {
 function secondsToMilliSeconds(seconds: number) {
 	return seconds * 1000;
 }
+
+function secondsToBeforeDateString(seconds: number) {
+	const MINUTE = 60;
+	const HOUR = MINUTE * 60;
+	const DAY = HOUR * 24;
+	const WEEK = DAY * 7;
+	const MONTH = WEEK * 4;
+	const YEAR = MONTH * 12;
+
+	if (seconds >= YEAR) {
+		return `${Math.floor(seconds / YEAR)}년 전`;
+	} else if (seconds >= MONTH) {
+		return `${Math.floor(seconds / MONTH)}달 전`;
+	} else if (seconds >= WEEK) {
+		return `${Math.floor(seconds / MONTH)}주 전`;
+	} else if (seconds >= DAY) {
+		return `${Math.floor(seconds / DAY)}일 전`;
+	} else if (seconds >= HOUR) {
+		return `${Math.floor(seconds / HOUR)}시간 전`;
+	} else {
+		return `${Math.floor(seconds / MINUTE)}분 전`;
+	}
+}
+
+export function getBeforeDate(dateString: string) {
+	const date = new Date(dateString);
+
+	const afterSeconds = convertUnixTimeToSeconds(Date.now() - date.getTime());
+
+	return secondsToBeforeDateString(afterSeconds);
+}
