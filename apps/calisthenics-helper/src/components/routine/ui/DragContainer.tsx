0195fd8ac19da9600dbe1534feb35d9ea6e19435
kimsg <ksg9396@gmail.com>
2025-03-19 17:24:49 +0900

    맨몸운동헬퍼 - drag 컨테이너 컴포넌트 추가

---
+++ apps/calisthenics-helper/src/components/routine/ui/DragContainer.tsx
@@ -0,0 +1,77 @@
+import {
+	DetailedHTMLProps,
+	DragEvent,
+	HTMLAttributes,
+	useRef,
+	useState,
+} from 'react';
+
+export default function DragContainer({
+	children,
+	className,
+	onDrop,
+	onDragStart,
+	...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
+	const [isDragEnter, setIsDragEnter] = useState<boolean>(false);
+	const dragStart = useRef<boolean>(false);
+
+	const handleDragStart = (e: DragEvent<HTMLDivElement>) => {
+		dragStart.current = true;
+		if (onDragStart) {
+			onDragStart(e);
+		}
+	};
+
+	const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
+		e.preventDefault();
+	};
+
+	const handleDragEnd = (e: DragEvent<HTMLDivElement>) => {
+		dragStart.current = false;
+	};
+
+	const handleDragEnter = (e: DragEvent<HTMLDivElement>) => {
+		if (dragStart.current) {
+			return;
+		}
+
+		setIsDragEnter(true);
+	};
+
+	const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
+		if (e.currentTarget.contains(e.relatedTarget as HTMLElement)) {
+			return;
+		}
+
+		setIsDragEnter(false);
+	};
+
+	const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+		e.preventDefault();
+		setIsDragEnter(false);
+
+		if (!dragStart.current && onDrop) {
+			onDrop(e);
+		}
+	};
+
+	return (
+		<div
+			{...props}
+			className={[
+				`${isDragEnter ? 'border border-primary' : 'border border-black'}`,
+				className,
+			].join(' ')}
+			draggable
+			onDragStart={handleDragStart}
+			onDragOver={handleDragOver}
+			onDragEnd={handleDragEnd}
+			onDrop={handleDrop}
+			onDragEnter={handleDragEnter}
+			onDragLeave={handleDragLeave}
+		>
+			{children}
+		</div>
+	);
+}
