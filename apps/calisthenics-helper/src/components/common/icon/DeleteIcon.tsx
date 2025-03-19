16d190113b21b8cb36b99beaa6f7d678e6d92560
kimsg <ksg9396@gmail.com>
2025-03-19 21:10:07 +0900

    맨몸운동헬퍼  - 삭제 아이콘 추가

---
+++ apps/calisthenics-helper/src/components/common/icon/DeleteIcon.tsx
@@ -0,0 +1,5 @@
+import { X } from 'lucide-react';
+
+export default function DeleteIcon() {
+	return <X className="text-error" />;
+}
--- apps/calisthenics-helper/src/components/common/icon/index.ts
+++ apps/calisthenics-helper/src/components/common/icon/index.ts
@@ -2,3 +2,4 @@ export { default as FlameIcon } from './FlameIcon';
 export { default as PauseIcon } from './PauseIcon';
 export { default as PlayIcon } from './PlayIcon';
 export { default as StopIcon } from './StopIcon';
+export { default as DeleteIcon } from './DeleteIcon';
