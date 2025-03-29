33451de481ec18df7ea4de87087c06984850e5c1
kimsg <ksg9396@gmail.com>
2025-03-29 18:25:01 +0900

    맨몸운동헬퍼 - 원격 이미지 저장소 주소 설정 추가

--- apps/calisthenics-helper/next.config.ts
+++ apps/calisthenics-helper/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	transpilePackages: ['@repo/ui'],
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'snvoclkontfqeiatywxv.supabase.co',
+			},
+		],
+	},
 };
 
 export default nextConfig;
