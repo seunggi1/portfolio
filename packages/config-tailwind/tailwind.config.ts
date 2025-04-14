9de20342b1192dc94bdc86c192d8950f2da42da5
kimsg <ksg9396@gmail.com>
2025-04-14 14:51:40 +0900

    Config - tailwind theme 속성 제거

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from 'tailwindcss';
 import daisyUI from 'daisyui';
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, 'content'> = {
-	theme: {
-		extend: {},
-	},
+const config: Omit<Config, 'content' | 'theme'> = {
 	daisyui: {
 		themes: ['light', 'dark', 'corporate'],
 	},
