11cc7d5b32c45112585e39da454e615d6a03cb4c
kimsg <ksg9396@gmail.com>
2025-04-14 18:17:12 +0900

    맨몸운동헬퍼 - 루틴 실행 페이지 스켈레톤 개선

--- apps/calisthenics-helper/src/components/routine/routine-run/RoutineRunnerSkeleton.tsx
+++ apps/calisthenics-helper/src/components/routine/routine-run/RoutineRunnerSkeleton.tsx
@@ -5,6 +5,7 @@ export default function RoutineRunnerSkeleton() {
 		<section
 			className={`flex flex-col items-center justify-center gap-4 h-full`}
 		>
+			<p className="text-xl font-bold">루틴을 불러오고 있습니다...</p>
 			<Skeleton className="w-1/2 h-60" />
 			<Skeleton className="w-1/2 h-10" />
 			<Skeleton className="w-1/2 h-10 mt-4" />
