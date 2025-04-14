f0480e93f2a3507d3867d95982abdc2f55215860
kimsg <ksg9396@gmail.com>
2025-04-14 22:08:30 +0900

    맨몸운동헬퍼 - 스켈레톤 컴포넌트 클라이언트 컴포넌트로 변경

--- apps/calisthenics-helper/src/components/routine/comment/CommentsSkeleton.tsx
+++ apps/calisthenics-helper/src/components/routine/comment/CommentsSkeleton.tsx
@@ -1,9 +1,10 @@
+'use client';
+
 import { Skeleton } from '@repo/ui/common';
 
 export default function CommentsSkeleton() {
 	return (
 		<section className="flex flex-col gap-4">
-			<Skeleton className="w-full h-8" />
 			<Skeleton className="w-full h-8" />
 			<Skeleton className="w-full h-8" />
 		</section>
