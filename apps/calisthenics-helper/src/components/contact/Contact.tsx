aa94e2a2744a3938ff3800a4e40c9efb7d32deb1
kimsg <ksg9396@gmail.com>
2025-04-23 21:52:01 +0900

    맨몸운동헬퍼 - 문의하기 공통 Form UI 사용

--- apps/calisthenics-helper/src/components/contact/Contact.tsx
+++ apps/calisthenics-helper/src/components/contact/Contact.tsx
@@ -2,7 +2,7 @@
 
 import { useActionState } from 'react';
 import { Button, Input, TextArea } from '@repo/ui/common';
-import { FormGroup, Loading } from '../common/ui';
+import { AuthForm, FormGroup, Loading } from '@/components/common/ui';
 import { nameofFactory } from '@/utils/type';
 import { ContactResponse } from '@/types/contact';
 import { createContactAction } from '@/actions';
@@ -20,53 +20,42 @@ export default function Contact() {
 	const nameof = nameofFactory<ContactResponse['inputs']>();
 
 	return (
-		<section className="w-full h-full bg-gray-100">
-			<div className="flex items-center h-full">
-				<form
-					action={formAction}
-					className="w-full p-4 mx-auto text-center bg-white md:!w-3/4"
-				>
-					<h2 className="mb-4 text-3xl font-bold text-center">문의 하기</h2>
-					<FormGroup
-						displayName="제목"
-						htmlFor={nameof('title')}
-						error={errors.title}
-					>
-						<Input
-							id={nameof('title')}
-							name={nameof('title')}
-							defaultValue={inputs.title}
-							minLength={2}
-							required
-						/>
-					</FormGroup>
-					<FormGroup
-						displayName="내용"
-						htmlFor={nameof('contents')}
-						error={errors.contents}
-					>
-						<TextArea
-							id={nameof('contents')}
-							defaultValue={inputs.contents}
-							name={nameof('contents')}
-							minLength={5}
-							required
-						/>
-					</FormGroup>
-					<FormGroup displayName="" addDivider={false}>
-						<Button
-							color="primary"
-							type="submit"
-							disabled={isPending || success}
-						>
-							{isPending ? <Loading /> : '문의 전송'}
-						</Button>
-						{success && (
-							<span className="text-success">성공적으로 전송되었습니다.</span>
-						)}
-					</FormGroup>
-				</form>
-			</div>
-		</section>
+		<AuthForm action={formAction}>
+			<h2 className="mb-4 text-3xl font-bold text-center">문의 하기</h2>
+			<FormGroup
+				displayName="제목"
+				htmlFor={nameof('title')}
+				error={errors.title}
+			>
+				<Input
+					id={nameof('title')}
+					name={nameof('title')}
+					defaultValue={inputs.title}
+					minLength={2}
+					required
+				/>
+			</FormGroup>
+			<FormGroup
+				displayName="내용"
+				htmlFor={nameof('contents')}
+				error={errors.contents}
+			>
+				<TextArea
+					id={nameof('contents')}
+					defaultValue={inputs.contents}
+					name={nameof('contents')}
+					minLength={5}
+					required
+				/>
+			</FormGroup>
+			<FormGroup displayName="" addDivider={false}>
+				<Button color="primary" type="submit" disabled={isPending || success}>
+					{isPending ? <Loading /> : '문의 전송'}
+				</Button>
+				{success && (
+					<span className="text-success">성공적으로 전송되었습니다.</span>
+				)}
+			</FormGroup>
+		</AuthForm>
 	);
 }
