b02966a0e2ccaa23a7751d22487a2a187fa37671
kimsg <ksg9396@gmail.com>
2025-04-24 23:37:17 +0900

    Admin - Login 페이지 트랜지션 추가

--- apps/admin/src/components/Login.tsx
+++ apps/admin/src/components/Login.tsx
@@ -1,34 +1,39 @@
+import { FormEvent, useState, useTransition } from 'react';
 import { login } from '@/api/stats';
 import { Button, Input } from '@repo/ui/common';
-import { useState } from 'react';
 
 type Props = {
 	onSuccess: () => void;
 };
 
 export default function Login({ onSuccess }: Props) {
+	const [isPending, startTransition] = useTransition();
 	const [error, setError] = useState<string>('');
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		startTransition(async () => {
+			e.preventDefault();
+
+			const formData = new FormData(e.target as HTMLFormElement);
+
+			const result = await login({
+				id: formData.get('id') as string,
+				password: formData.get('password') as string,
+			});
+
+			if (result) {
+				onSuccess();
+			} else {
+				setError('올바르지않은 인증 정보 입니다.');
+			}
+		});
+	};
 
 	return (
 		<section className="flex items-center justify-center w-full h-full bg-gray-100">
 			<form
 				className="flex flex-col justify-center w-full gap-2 px-4 py-2 bg-white rounded-md md:!w-1/2"
-				onSubmit={async (e) => {
-					e.preventDefault();
-
-					const formData = new FormData(e.target as HTMLFormElement);
-
-					const result = await login({
-						id: formData.get('id') as string,
-						password: formData.get('password') as string,
-					});
-
-					if (result) {
-						onSuccess();
-					} else {
-						setError('올바르지않은 인증 정보 입니다.');
-					}
-				}}
+				onSubmit={handleSubmit}
 			>
 				<h2 className="text-2xl font-bold text-center">로그인</h2>
 				<label htmlFor="id">관리자 아이디</label>
@@ -50,7 +55,9 @@ export default function Login({ onSuccess }: Props) {
 					required
 				/>
 				{error && <span className="text-error">{error}</span>}
-				<Button type="submit">로그인</Button>
+				<Button disabled={isPending} type="submit">
+					로그인
+				</Button>
 			</form>
 		</section>
 	);
